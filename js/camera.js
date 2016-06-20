cbc0eadca08673c9242e52ed816844266170c5bb
akosua kernizan <akernizan@gmail.com>
2016-06-19 20:28:06 -0400

    aded buttons to turn on and off webcam

--- js/camera.js
+++ js/camera.js
@@ -1,12 +1,24 @@
 var video = document.querySelector('#videoElement');
+var button = document.querySelector('#on');
+var buttonOff = document.querySelector('#off');
 
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
-if (navigator.getUserMedia) {
-	navigator.getUserMedia({
-		video: true
-	}, handleVideo, videoError);
-}
+
+button.addEventListener('click', function () {
+	if (navigator.getUserMedia) {
+		navigator.getUserMedia({
+			video: true
+		}, handleVideo, videoError);
+	}
+});
+
+buttonOff.addEventListener('click', function () {
+	video.pause();
+	video.src = "";
+	localstream.getTracks()[0].stop();
+})
+
 
 function handleVideo(stream) {
 	video.src = window.URL.createObjectURL(stream)
